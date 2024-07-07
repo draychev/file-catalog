3f3b346ca5f2bac0b3d26c21dd24bdb34d311136
Delyan Raychev <delyanr.com>
2024-07-07 13:44:36 -0700

    Hash files in parallel

--- pkg/commands/hash.go
+++ pkg/commands/hash.go
@@ -2,6 +2,8 @@ package commands
 
 import (
 	"fmt"
+	"runtime"
+	"sync"
 
 	"github.com/spf13/cobra"
 
@@ -23,18 +25,45 @@ func GetHash(outputPath, storagePath string) *cobra.Command {
 
 			log.Info().Msgf("Here are the fileops we found in %s: %+v", storagePath, files)
 
+			numCPU := runtime.NumCPU()
+			var wg sync.WaitGroup
+			fileChan := make(chan string, len(files))
+			metaChan := make(chan fileops.FileMeta, len(files))
+
+			wg.Add(numCPU)
+
+			for i := 0; i < numCPU; i++ {
+				go func() {
+					defer wg.Done()
+					for file := range fileChan {
+						meta, err := fileops.GetMetadata(file)
+						if err != nil {
+							log.Error().Err(err).Msgf("Error getting fileops metadata: %s", err)
+							continue
+						}
+						metaChan <- meta
+					}
+				}()
+			}
+
+			go func() {
+				for _, file := range files {
+					fileChan <- file
+				}
+				close(fileChan)
+				wg.Wait()
+				close(metaChan)
+			}()
+
 			var metas []fileops.FileMeta
 			totalFiles := len(files)
+			processedFiles := 0
 
-			for i, file := range files {
-				meta, err := fileops.GetMetadata(file)
-				if err != nil {
-					log.Error().Err(err).Msgf("Error getting fileops metadata: %s", err)
-					continue
-				}
+			for meta := range metaChan {
 				metas = append(metas, meta)
-				percentComplete := float64(i+1) / float64(totalFiles) * 100
-				fmt.Printf("%.2f%% - Hashing fileops: %s\n", percentComplete, file)
+				processedFiles++
+				percentComplete := float64(processedFiles) / float64(totalFiles) * 100
+				fmt.Printf("%.2f%% - Hashing fileops: %s\n", percentComplete, meta.FileName)
 			}
 
 			if err := storage.SerializeFileMeta(metas, outputPath); err != nil {
